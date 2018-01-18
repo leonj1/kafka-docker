e8b05481ef8fef8bc93f97a5385b3c0c33fc0f92
Jose Leon <leonj1@gmail.com>
2018-01-18 00:18:28 -0500

    Adding debugging

--- start-kafka.sh
+++ start-kafka.sh
@@ -37,8 +37,10 @@ do
     kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
     env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
     if egrep -q "(^|^#)$kafka_name=" $KAFKA_HOME/config/server.properties; then
+        echo  env_var1 is ${!env_var}
         sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
     else
+        echo  env_var2 is ${!env_var}
         echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
     fi
   fi
